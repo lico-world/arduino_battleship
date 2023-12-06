20fcae552e02ce24bed65f4dc75d50b93c41d3dd
lico <107700554+lico-world@users.noreply.github.com>
2023-12-06 21:25:15 +0100

    Implementation of reading and debugging for boolean arrays
    These two boolean arrays are player and opponent game boards.

---
+++ battleship.c
@@ -0,0 +1,46 @@
+const int GAME_BOARD_SIZE = 5;
+bool PLAYER_ARRAY[GAME_BOARD_SIZE * GAME_BOARD_SIZE] = {true, false, false, false, true, false, true, true, false, true, true, false, false, false, true, false, true, true, false, true, false, true, true, false, true};
+bool OPPONENT_ARRAY[GAME_BOARD_SIZE * GAME_BOARD_SIZE] = {true, false, false, false, true, false, true, true, false, true, false, false, false, true, true, false, true, false, true, true, false, false, false, true, false};;
+bool * CURRENT_ARRAY;
+
+bool isDisplaySubjectThePlayer = true;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  CURRENT_ARRAY = PLAYER_ARRAY;
+
+  // Verify the array reading navigation functionality
+  debugCurrentArray();
+  changeArray();
+  debugCurrentArray();
+}
+
+void loop()
+{
+
+}
+
+void debugCurrentArray()
+{
+  for (int i = 0 ; i < GAME_BOARD_SIZE ; i++)
+  {
+    for (int j = 0 ; j < GAME_BOARD_SIZE ; j++)
+    {
+      Serial.print(CURRENT_ARRAY[j + GAME_BOARD_SIZE * i] ? '#' : '~');
+    }
+    Serial.print("\n");
+  }
+  Serial.print("\n");
+}
+
+void changeArray()
+{
+  // Invert the flag that discriminate who between player and opponent should be displayed
+  isDisplaySubjectThePlayer = !isDisplaySubjectThePlayer;
+
+  // Select the good array
+  if(isDisplaySubjectThePlayer) CURRENT_ARRAY = PLAYER_ARRAY;
+  else CURRENT_ARRAY = OPPONENT_ARRAY;
+}

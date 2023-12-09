7de5a7a6821c4f3312faaa985998d3895812afc4
lico <107700554+lico-world@users.noreply.github.com>
2023-12-09 20:52:43 +0100

    Input implementation
    Input logic: press when you are in the good row then release when you are on the good line

--- battleship.c
+++ battleship.c
@@ -2,27 +2,37 @@
 
 #define GAME_BOARD_SIDE_SIZE 10
 #define LED_PIN 6
+#define BUTTON_PIN 7
 
+// Size of Game Boards arrays
 const int GAME_BOARD_SIZE = GAME_BOARD_SIDE_SIZE * GAME_BOARD_SIDE_SIZE;
+int verticalLine;
 
+// Game Boards arrays
 bool PLAYER_ARRAY[GAME_BOARD_SIZE] = {false};
 bool OPPONENT_ARRAY[GAME_BOARD_SIZE] = {false};
 bool * CURRENT_ARRAY; // Will point toward either PLAYER_ARRAY or OPPONENT_ARRAY
 
-bool isPlayerTurn = true; // /!\ THE ONLY VARIABLE TO CHANGE BETWEEN THE TWO BOARDS
+// /!\ THE ONLY VARIABLE TO CHANGE BETWEEN THE TWO BOARDS
+bool isPlayerTurn = true;
 
 Adafruit_NeoPixel screen(GAME_BOARD_SIZE, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+enum ORIENTATION {HORIZONTAL, VERTICAL};
 
 void setup()
 {
   Serial.begin(9600);
+
+  pinMode(6, OUTPUT);
+  pinMode(7, INPUT);
 
   screen.begin();
   screen.clear();
   screen.setBrightness(255);
 
   // Select the current array according to the value of 'isPlayerTurn' variable
-  CURRENT_ARRAY = isPlayerTurn ? PLAYER_ARRAY : OPPONENT_ARRAY;
+  CURRENT_ARRAY = isPlayerTurn ? OPPONENT_ARRAY : PLAYER_ARRAY;
 }
 
 void loop()
@@ -33,8 +43,11 @@ void loop()
 
     do
     {
-      x = waitInput();
-      y = waitInput();
+      y = waitInput(VERTICAL);
+      verticalLine = y;
+      delay(200);
+      x = waitInput(HORIZONTAL);
+      verticalLine = 0;
     } while(!playOn(x, y)); // Only accept valid plays
   }
   else // Opponent is playing
@@ -42,27 +55,46 @@ void loop()
     waitOpponentPlay();
   }
 
-  displayCurrentArray();
+  displayCurrentArray(true);
   debugCurrentArray();
-  changeArray();
+  changeArray(); // Change turn
 }
 
 void waitOpponentPlay()
 {
-  // delay for proof of concept
-  delay(1000);
+  // Random value for proof of concept
+  int x, y;
+  do
+  {
+    x = random(9);
+    y = random(9);
+  }while(PLAYER_ARRAY[y + GAME_BOARD_SIDE_SIZE * x]);
+
+  PLAYER_ARRAY[y + GAME_BOARD_SIDE_SIZE * x] = true;
+
+  delay(500);
 }
 
-int waitInput()
+int waitInput(ORIENTATION orientation)
 {
-  // random value for proof of concept
-  delay(1000);
-  return random(GAME_BOARD_SIDE_SIZE-1);
+  int coord;
+  drawLine(coord, orientation); // draw the first line
+
+  // Push for first coordinate, release for the second one
+  while(digitalRead(7) != (orientation == VERTICAL ? HIGH : LOW))
+  {
+    coord = (coord + 1) % GAME_BOARD_SIDE_SIZE;
+    drawLine(coord, orientation); // Draw the line moving
+    delay(500); // Delay for the line speed
+  }
+
+  return coord;
 }
 
-void displayCurrentArray()
+// 'displayFlag' is used to disable clear and show calls (useful for drawLine)
+void displayCurrentArray(bool displayFlag)
 {
-  screen.clear();
+  if(displayFlag) screen.clear();
   for(int i = 0 ; i < GAME_BOARD_SIDE_SIZE ; i++)
   {
     for(int j = 0 ; j < GAME_BOARD_SIDE_SIZE ; j++)
@@ -70,10 +102,10 @@ void displayCurrentArray()
       int coord = j + GAME_BOARD_SIDE_SIZE * i;
 
       // Light the LED only if the bool is true on this coordinates
-      if(CURRENT_ARRAY[coord]) screen.setPixelColor(coord, 255, 0, 0);
+      if(CURRENT_ARRAY[coord]) screen.setPixelColor(coord, 127, 0, 0);
     }
   }
-  screen.show();
+  if(displayFlag) screen.show();
 }
 
 void debugCurrentArray()
@@ -95,7 +127,7 @@ void changeArray()
   isPlayerTurn = !isPlayerTurn;
 
   // Select the good array
-  if(isPlayerTurn) CURRENT_ARRAY = PLAYER_ARRAY;
+  if(!isPlayerTurn) CURRENT_ARRAY = PLAYER_ARRAY;
   else CURRENT_ARRAY = OPPONENT_ARRAY;
 }
 
@@ -103,8 +135,42 @@ bool playOn(int x, int y)
 {
   // If the play is not valid
   if (OPPONENT_ARRAY[y + GAME_BOARD_SIDE_SIZE * x] == true) return false; 
-
+  
   // Play on the provided coordinates
   OPPONENT_ARRAY[y + GAME_BOARD_SIDE_SIZE * x] = true;
   return true;
+}
+
+void drawLine(int linePosition, ORIENTATION orientation)
+{
+  // Display the already played locations
+  displayCurrentArray(true);
+
+  // Since it's always VERTICAL then HORIZONTAL
+  // Display the first line when the second on is moving
+  if(orientation == HORIZONTAL)
+  {
+    for(int i=0 ; i<GAME_BOARD_SIDE_SIZE ; i++)
+      screen.setPixelColor(verticalLine + GAME_BOARD_SIDE_SIZE * i, screen.getPixelColor(verticalLine + GAME_BOARD_SIDE_SIZE * i) + 32512);
+  }
+
+  for(int i=0 ; i<GAME_BOARD_SIDE_SIZE ; i++)
+  {
+    int coord;
+
+    // Select the line orientation
+    switch(orientation)
+    {
+      case HORIZONTAL:
+        coord = linePosition * GAME_BOARD_SIDE_SIZE + i;
+        break;
+      case VERTICAL:
+        coord = linePosition + GAME_BOARD_SIDE_SIZE * i;
+        break;
+    }
+
+    // Add 127 to the green value of the pixel
+    screen.setPixelColor(coord, screen.getPixelColor(coord) + 32512);
+  }
+  screen.show();
 }
